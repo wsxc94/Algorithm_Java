a41aff02f2dcb686323b95126e69ce013bbec936
안영훈 <wsxc9468@gmail.com>
2022-05-13 17:04:00 +0900

    Level 1 신규 아이디 추천 - 정규 표현식

---
+++ src/신규아이디추천.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class 신규아이디추천 {
+    public static void main(String[] args) {
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+
+
+        String answer = new_id.toLowerCase(); // 1단계
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        answer = answer.replaceAll("[.]{2,}", "."); // 3단계
+        answer = answer.replaceAll("^[.]|[.]$", "");    // 4단계
+
+        if (answer.equals("")) {    // 5단계
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {     // 6단계
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
+        }
+
+        if (answer.length() <= 2) {  // 7단계
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+
+        System.out.println("answer = " + answer);
+    }
+}
